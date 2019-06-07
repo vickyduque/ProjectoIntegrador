54e59f65c8c352d3efa563517df9c80a7bcdbd68
Sofia <smollon@udesa.edu.ar>
2019-06-07 12:03:16 -0300

    Progress!!

--- ProyectoIntegrados.js
+++ ProyectoIntegrados.js
@@ -1,12 +1,14 @@
 window.onload = function(){
 
-  fetch("https://api.themoviedb.org/3/movie/upcoming?api_key=d6ff71a6bd2a94eaac5b986a9112d505&page=1")
+  // Extraemos api de peliculas populares
+
+  fetch("https://api.themoviedb.org/3/movie/popular?api_key=d6ff71a6bd2a94eaac5b986a9112d505&page=1")
     .then(function(respuesta) {
       return respuesta.json()
     })
     .then(function(informacion) {
       console.log(informacion);
-
+console.log("popular");
       var arrayPeliculas = informacion.results
       console.log(arrayPeliculas);
 
@@ -15,8 +17,8 @@ window.onload = function(){
         var title = arrayPeliculas[i].title
         var poster = arrayPeliculas[i].poster_path
 
-        
-        document.querySelector("div.peliculas").innerHTML += "<img src=https://image.tmdb.org/t/p/w500" + poster+ ">"
+        document.querySelector("ul#popular").innerHTML += "<li> <img class=uk-align-center src=https://image.tmdb.org/t/p/w500" + poster + " 'uk-cover'> </li>"
+
       }
 
     })
@@ -24,7 +26,59 @@ window.onload = function(){
       console.log("Error: " + error);
     })
 
+      // Extraemos api de peliculas con mayor puntaje
 
+    fetch("https://api.themoviedb.org/3/movie/top_rated?api_key=d6ff71a6bd2a94eaac5b986a9112d505&page=1")
+      .then(function(respuesta) {
+        return respuesta.json()
+      })
+      .then(function(informacion) {
+        console.log(informacion);
+console.log("ranking");
+        var arrayPeliculas = informacion.results
+        console.log(arrayPeliculas);
+
+        for (var i = 0; i < arrayPeliculas.length; i++) {
+          var id = arrayPeliculas[i].id
+          var title = arrayPeliculas[i].title
+          var poster = arrayPeliculas[i].poster_path
+
+          document.querySelector("ul#top").innerHTML += "<li> <img class=uk-align-center src=https://image.tmdb.org/t/p/w500" + poster + " 'uk-cover'> </li>"
+
+
+        }
+
+      })
+      .catch(function(error) {
+        console.log("Error: " + error);
+      })
+
+      // Extraemos api de peliculas a estrenar
+
+    fetch("https://api.themoviedb.org/3/movie/upcoming?api_key=d6ff71a6bd2a94eaac5b986a9112d505&page=1")
+      .then(function(respuesta) {
+        return respuesta.json()
+      })
+      .then(function(informacion) {
+        console.log(informacion);
+console.log("estrenos");
+        var arrayPeliculas = informacion.results
+        console.log(arrayPeliculas);
+
+        for (var i = 0; i < arrayPeliculas.length; i++) {
+          var id = arrayPeliculas[i].id
+          var title = arrayPeliculas[i].title
+          var poster = arrayPeliculas[i].poster_path
+console.log("https://image.tmdb.org/t/p/w500" + poster);
+          document.querySelector("ul#upcoming").innerHTML += "<li> <img class=uk-align-center src=https://image.tmdb.org/t/p/w500" + poster + " 'uk-cover'> </li>"
+
+
+        }
+
+      })
+      .catch(function(error) {
+        console.log("Error: " + error);
+      })
 
 
 
