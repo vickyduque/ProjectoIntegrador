adabf889d3ce9b9c696c8f9bda52e9742786a366
vicky <vickyduque12@gmail.com>
2019-06-14 11:21:10 -0300

    pagina2, falta css

---
+++ pagina2.js
@@ -0,0 +1,29 @@
+window.onload=function (){
+
+  // Extraemos api de genero de peliculas
+ fetch("https://api.themoviedb.org/3/genre/movie/list?api_key=d6ff71a6bd2a94eaac5b986a9112d505&page=1")
+  .then(function(respuesta) {
+        return respuesta.json()
+      })
+  .then(function(informacion) {
+  console.log(informacion);
+
+  var generos= informacion.genres
+  console.log(generos);
+
+
+  for (var i = 0; i < generos.length; i++) {
+    var id = generos[i].id
+    var nombre = generos[i].name
+
+    document.querySelector("#generos").innerHTML += "<a class=uk-text-center href=pagina3.html?id=" + id + ">" + nombre + "</a><br>"
+  }
+})
+
+  .catch(function(error) {
+   console.log("Error: " + error);
+      })
+
+    }
+  //var idgenero = new URLSearchParams (location.search).get ("id")
+  //"<a class=uk-text-center href=pagina2.html?id=" + idgenero +"</a>"
